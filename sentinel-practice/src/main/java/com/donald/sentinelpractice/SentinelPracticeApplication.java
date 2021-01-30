ee403d92aeb8fb4a41d474b10921ad51aef9b9d0
Donald <448641125@qq.com>
2021-01-30 23:55:56 +0800

    add sentinel demo

---
+++ sentinel-practice/src/main/java/com/donald/sentinelpractice/SentinelPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.donald.sentinelpractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SentinelPracticeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SentinelPracticeApplication.class, args);
+    }
+
+}
