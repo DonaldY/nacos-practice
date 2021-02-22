3bddfc22a5b1b17220f874ffe7e45df522a111c8
Donald <448641125@qq.com>
2021-02-22 21:35:33 +0800

    update consumer controller demo

--- nacos-consumer/src/main/java/com/donald/nacosconsumer/controller/HelloController.java
+++ nacos-consumer/src/main/java/com/donald/nacosconsumer/controller/HelloController.java
@@ -4,6 +4,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
@@ -51,4 +53,11 @@ class HelloController {
                         "/echo?name=nacos", String.class);
     }
 
+    @PostMapping("/hello/1")
+    public String hello1(@RequestBody Object o) {
+
+        System.out.println(o.toString());
+
+        return "123";
+    }
 }
