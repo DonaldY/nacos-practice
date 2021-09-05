1f815f0f14420b34f07f18b33e9a10a4a6fc2fed
Donald <448641125@qq.com>
2021-09-05 17:14:19 +0800

    init hystrix helloWorld demo

---
+++ hystrix-practice/src/main/java/com/donald/hystrixpriactice/command/HelloWorldCommand.java
@@ -0,0 +1,34 @@
+package com.donald.hystrixpriactice.command;
+
+import com.netflix.hystrix.HystrixCommand;
+import com.netflix.hystrix.HystrixCommandGroupKey;
+import com.netflix.hystrix.HystrixCommandProperties;
+import org.springframework.web.client.RestTemplate;
+
+/**
+ * @author donald
+ * @date 2021/09/03
+ */
+public class HelloWorldCommand extends HystrixCommand<String> {
+
+    private final RestTemplate restTemplate = new RestTemplate();
+    private final String code;
+
+    public HelloWorldCommand(String code) {
+        super(HystrixCommandGroupKey.Factory.asKey("HelloWorldExample"));
+        this.code = code;
+    }
+
+    @Override
+    protected String run() {
+        String url = "http://httpbin.org/status/" + code;
+        System.out.println("start to curl: " + url);
+        restTemplate.getForObject(url, String.class);
+        return "Request success";
+    }
+
+    @Override
+    protected String getFallback() {
+        return "Request failed";
+    }
+}
