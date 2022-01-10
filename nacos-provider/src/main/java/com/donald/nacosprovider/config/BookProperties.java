5a4543909e67954fd17df72828da94e3084eb2d5
Donald <448641125@qq.com>
2022-01-10 11:46:20 +0800

    update bookProperties

--- nacos-provider/src/main/java/com/donald/nacosprovider/config/BookProperties.java
+++ nacos-provider/src/main/java/com/donald/nacosprovider/config/BookProperties.java
@@ -12,10 +12,21 @@ import org.springframework.context.annotation.Configuration;
 public class BookProperties {
 
     private String category;
-
     private String author;
-
     private String name;
+
+    // 注意：需要有 setter 方法，参数注入才有效
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 
     @Override
     public String toString() {
