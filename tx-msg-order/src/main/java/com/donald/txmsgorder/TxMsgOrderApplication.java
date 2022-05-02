71d3327d16bbd4b665183f6935b91c244954304b
Donald <448641125@qq.com>
2022-05-02 16:22:27 +0800

    Update: add tx-msg-order

---
+++ tx-msg-order/src/main/java/com/donald/txmsgorder/TxMsgOrderApplication.java
@@ -0,0 +1,13 @@
+package com.donald.txmsgorder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TxMsgOrderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TxMsgOrderApplication.class, args);
+    }
+
+}
---
+++ tx-msg-order/src/test/java/com/donald/txmsgorder/TxMsgOrderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.donald.txmsgorder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TxMsgOrderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
