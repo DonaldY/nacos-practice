de785ddf660dc5ca0b16c2998581fdd4f3c59ab1
Donald <448641125@qq.com>
2022-05-08 02:36:35 +0800

    Update: init tx-notifymsg-account

---
+++ tx-notifymsg-account/src/main/java/com/donald/txnotifymsgaccount/TxNotifymsgAccountApplication.java
@@ -0,0 +1,13 @@
+package com.donald.txnotifymsgaccount;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TxNotifymsgAccountApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TxNotifymsgAccountApplication.class, args);
+    }
+
+}
---
+++ tx-notifymsg-account/src/test/java/com/donald/txnotifymsgaccount/TxNotifymsgAccountApplicationTests.java
@@ -0,0 +1,13 @@
+package com.donald.txnotifymsgaccount;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TxNotifymsgAccountApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
