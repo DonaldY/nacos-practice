b00ea391fa943cf3c28dd328eab68768696df2f8
Donald <448641125@qq.com>
2022-05-07 09:18:22 +0800

    Update: update tx-msg-order mapper

--- tx-msg-order/src/main/java/com/donald/txmsgorder/TxMsgOrderApplication.java
+++ tx-msg-order/src/main/java/com/donald/txmsgorder/TxMsgOrderApplication.java
@@ -1,10 +1,12 @@
 package com.donald.txmsgorder;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
+@MapperScan(value = { "com.donald.txmsgorder.mapper" })
 @EnableTransactionManagement(proxyTargetClass = true)
 public class TxMsgOrderApplication {
 
