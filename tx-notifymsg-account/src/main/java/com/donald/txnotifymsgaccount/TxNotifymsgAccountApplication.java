1e624984d65a2b9754358e4dd5564e6c325aa6ca
Donald <448641125@qq.com>
2022-05-09 18:58:40 +0800

    Update: update tx-notifymsg-account sql

--- tx-notifymsg-account/src/main/java/com/donald/txnotifymsgaccount/TxNotifymsgAccountApplication.java
+++ tx-notifymsg-account/src/main/java/com/donald/txnotifymsgaccount/TxNotifymsgAccountApplication.java
@@ -1,8 +1,13 @@
 package com.donald.txnotifymsgaccount;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
+
+@MapperScan(value = { "com.donald.txnotifymsgaccount.mapper" })
+@EnableTransactionManagement(proxyTargetClass = true)
 @SpringBootApplication
 public class TxNotifymsgAccountApplication {
 
